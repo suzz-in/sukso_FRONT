5398793b60e31a34e1d44ef50f6adcf5bdde698a
suzz-in <o_o_su_jin_@naver.com>
2022-09-07 00:06:38 +0900

    로그인재수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,34 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL:
-    "https://a358bdec-994d-43d3-9c5d-094e23523748.mock.pstmn.io/api/user",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-});
+//axios.defaults.baseURL=
+
+// const API = axios.create({
+//   baseURL:
+//     "https://a358bdec-994d-43d3-9c5d-094e23523748.mock.pstmn.io/api/user",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//});
+
+const refresh = false;
+axios.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response.status === 400 && !refresh) {
+      refresh = true;
+      const response = await axios.post(
+        "refresh",
+        {},
+        { withCredentials: true }
+      );
+      if (response.status === 200) {
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${response.payload.accessToken}`;
+        return axios(error.config);
+      }
+    }
+  }
+);
 
 export default API;
